56f5cd5309d33a67e1ad3fc63c0d415d8e98fd1c
Cao Nguyen V <cao.nguyen-fr@asiantech.vn>
2018-09-13 08:42:22 +0700

    fix detail frontend

--- resources/views/public/detail.blade.php
+++ resources/views/public/detail.blade.php
@@ -2,31 +2,31 @@
 @section('content')
 <!-- start main -->
 <div class="main_bg">
-	<div class="wrap">
+	<div class="detail-wrap">
 		<div class="blog">
 			<!-- start main_content -->
 			<div class="blog_left">
 				<h3 class="style">{{ $category->name_cat }}</h3>
 				<div class="extra">
 					Địa điểm: {{ $category->location }}<br />
-					Đăng bởi: <span class="username">{{ $user->fullname }}</span> | 
+					Đăng bởi: <span class="username">{{ $user->username }}</span> | 
 				<time pubdate="">{{ $post->time_create }}</time>
 				</div>
-				<img src="{{ asset('template/public/images/slider1.jpg') }}" style="width:600px;height:300px;">
-				<div class="links" style="text-align:justify">{{ $post->preview }}</div>
-				<div class="links" style="text-align:justify">{{ $post->detail }}</div> 	
+				<img src="{{ asset('template/public/images/slider1.jpg') }}">
+				<div class="" style="text-align:justify">{{ $post->preview }}</div><hr />
+				<div class="" style="text-align:justify">{{ $post->detail }}</div>
 			</div>
 			<div class="sidebar">	
-					<h3>Danh mục</h3>
-					<ul class="s_nav">
-					@foreach(getCategories() as $category)
-						<li>
-							<a href="">
-								<span>{{ $category->name_cat }}</span><label>{{ $category->num_posts }}</label><div class="clear"></div>
-							</a>
-						</li>
-					@endforeach
-					</ul>	
+				<h3>Danh mục</h3>
+				<ul class="s_nav">
+				@foreach(getCategories() as $category)
+					<li>
+						<a href="">
+							<span>{{ $category->name_cat }}</span><label>{{ $category->num_posts }}</label><div class="clear"></div>
+						</a>
+					</li>
+				@endforeach
+				</ul>
 			</div>
 			<div class="clear"></div>
 			<!-- end main_content -->
